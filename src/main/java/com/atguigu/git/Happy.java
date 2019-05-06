c9bba51385623ebe6b82957bd1a2ab471031a222
eclipse_user <eclipse_user@atguigu.com>
2019-05-06 21:08:14 +0800

    楼主我自己修炼了葵花宝典

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -4,5 +4,6 @@ public class Happy {
 	
 	public static void main(String[] args) {
 		System.out.println("=======test the java test java ===========");
+		System.out.println("=======葵花宝典你练了没有啊？ ===========");
 	}
 }
