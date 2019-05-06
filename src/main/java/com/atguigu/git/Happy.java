5c914b4f3883fd5f060929f4900d14d3550c393d
eclipse_user <eclipse_user@atguigu.com>
2019-05-06 22:19:31 +0800

    edit by hot_fix brantch

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -6,5 +6,6 @@ public class Happy {
 		System.out.println("=======test the java test java ===========");
 		System.out.println("=======葵花宝典你练了没有啊？ ===========");
 		System.out.println("======我是 下载你的代码来使用的那一位同事====");
+		System.out.println("====当前时间点是什么呢？======");
 	}
 }
