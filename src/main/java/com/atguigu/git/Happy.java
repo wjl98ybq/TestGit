33bb36bb3743069b98178141fedb29ecb11b8a3b
wenjunlong88_glb <wenjunlong88_easy@163.com>
2019-05-06 20:49:02 +0800

    下载你的代码的那一位同事第一次提交代码

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -4,5 +4,6 @@ public class Happy {
 	
 	public static void main(String[] args) {
 		System.out.println("=======test the java test java ===========");
+		System.out.println("======我是 下载你的代码来使用的那一位同事====");
 	}
 }
